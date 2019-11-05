f56f5ac9f8a69357566f5e3f81b609db4d776a98
Alex Ricardo <Alex.RiCardo1999@hotmail.com>
2019-11-05 01:32:28 -0300

    Interface de CADASTRO inclusa

---
+++ cadastro.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt">
+<title>SISTEMA DE LOGIN ORIENTADO A OBJETO</title>
+<meta charset="utf-8">
+<link rel="stylesheet" type="text/css" href="css/index.css">
+<body>
+
+    
+
+    <div id="corpo">
+        <h1>CADASTRA-SE</h1>
+        <form action="efetuarLogin.php" method="" >
+            <input type="email" name="email" class="placeholder" placeholder="Preencha seu E-mail">
+            <input type="text" name="nome" class="placeholder" placeholder="Preencha seu Primeiro Nome">
+            <input type="password" name="senha" class="placeholder" placeholder="Preencha sua Senha">
+            <input type="password" name="" class="placeholder" placeholder="Confirmar senha">
+            <input type="submit" value="CADASTRAR">
+            <a href="index.php" >Já tem uma conta? <b> Clica aqui para acessar!</b> </a>
+        </form> 
+    </div>
+
+</body>
+</html>
--- index.php
+++ index.php
@@ -10,12 +10,15 @@
     <div id="corpo">
         <h1>SISTEMA DE LOGIN ORIENTADO A OBJETO</h1>
         <form action="efetuarLogin.php" method="" >
-            <input type="email" name="" placeholder="Preencha seu E-mail">
-            <input type="password" name="" placeholder="Preencha sua Senha">
+            <input type="email" name="" id="email" placeholder="Preencha seu E-mail">
+            <input type="password" name="" id="senha" placeholder="Preencha sua Senha">
             <input type="submit" value="ENTRAR">
-            <a href="" >Ainda não é inscrito? <b> Cadastrar-se!</b> </a>
+            <a href="cadastro.php" >Ainda não é inscrito? <b> Cadastrar-se!</b> </a>
         </form> 
     </div>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script type="text/javascript" src="jqueryValidation"></script>
 
 </body>
 </html>
