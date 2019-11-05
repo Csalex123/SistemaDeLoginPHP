8996c9afb406c83bf5edabc8d47068ffd9ca89d4
Alex Ricardo <Alex.RiCardo1999@hotmail.com>
2019-11-05 01:01:58 -0300

    Interface de login inclusa

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt">
+<title>SISTEMA DE LOGIN ORIENTADO A OBJETO</title>
+<meta charset="utf-8">
+<link rel="stylesheet" type="text/css" href="css/index.css">
+<body>
+
+    
+
+    <div id="corpo">
+        <h1>SISTEMA DE LOGIN ORIENTADO A OBJETO</h1>
+        <form action="efetuarLogin.php" method="" >
+            <input type="email" name="" placeholder="Preencha seu E-mail">
+            <input type="password" name="" placeholder="Preencha sua Senha">
+            <input type="submit" value="ENTRAR">
+            <a href="" >Ainda não é inscrito? <b> Cadastrar-se!</b> </a>
+        </form> 
+    </div>
+
+</body>
+</html>
